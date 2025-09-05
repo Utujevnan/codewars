ec63167f2dc650161d2cd42a48a4734f67cc3eca
Utuje Vanessa <utujevan@gmail.com>
2025-09-05 11:57:07 +0200

    reverse funcion

--- reverse.js
+++ reverse.js
@@ -1,3 +1,13 @@
-export const digitize = (n: number): number[] => {
-  return n.toString().split("").reverse().map(Number); // <== this converts each string to number
-};
+function reverse(str) {
+  let trimmed = str.trim();
+
+  let words = trimmed.split(/\s+/);
+
+  for (let i = 0; i < words.length; i++) {
+    if (i % 2 === 1) {
+      words[i] = words[i].split("").reverse().join("");
+    }
+  }
+
+  return words.join(" ");
+}
